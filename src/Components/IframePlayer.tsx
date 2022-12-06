7d697ccf1699eac3c4a3a3c8edc81884dfaf8b57
JLuis Avalos <luizavalos40@gmail.com>
2022-12-06 15:33:26 -0600

    iframe player responsive

--- src/Components/IframePlayer.tsx
+++ src/Components/IframePlayer.tsx
@@ -1,12 +1,17 @@
 function IframePlayer({ clipUrl }: IframePlayer) {
   return (
-    <iframe
-      src={`${clipUrl}&parent=${window.location.hostname}`}
-      allowFullScreen
-      height="720"
-      width="1280"
-      loading="eager"
-    ></iframe>
+    <div
+      style={{ width: "100%", height: "50vh" }}
+      className="embed-responsive embed-responsive-16by9 d-flex justify-content-center"
+    >
+      <iframe
+        style={{ width: "100%", height: "100%" }}
+        className="embed-responsive-item"
+        src={`${clipUrl}&parent=${window.location.hostname}`}
+        allowFullScreen
+        loading="eager"
+      ></iframe>
+    </div>
   );
 }
 
