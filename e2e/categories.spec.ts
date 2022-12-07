4d5773c30bb0324d6f5910d1a84ba0f6ea3b87b1
JLuis Avalos <luizavalos40@gmail.com>
2022-12-06 19:07:27 -0600

    testing working with github actions

--- e2e/categories.spec.ts
+++ e2e/categories.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from "@playwright/test";
 test("Input to be visible in categories", async ({ page }) => {
   await page.goto("http://localhost:5173/");
   const input = page.locator("input[type=text]");
-  await expect(input).toBeHidden();
+  await expect(input).toBeVisible();
 });
