5be06678b24764e688a0fa7e00120d00d9cfa0f3
JLuis Avalos <luizavalos40@gmail.com>
2022-12-06 19:05:11 -0600

    fail test on purpose

--- e2e/categories.spec.ts
+++ e2e/categories.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from "@playwright/test";
 test("Input to be visible in categories", async ({ page }) => {
   await page.goto("http://localhost:5173/");
   const input = page.locator("input[type=text]");
-  await expect(input).toBeVisible();
+  await expect(input).toBeHidden();
 });
