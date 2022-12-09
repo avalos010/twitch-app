a0634e0d5c2c512e7462049ab9a8888c4361ef35
JLuis Avalos <luizavalos40@gmail.com>
2022-12-08 19:57:25 -0600

    remove .only

--- e2e/channels.spec.ts
+++ e2e/channels.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect, Page } from "@playwright/test";
 
 test.describe("Channels", () => {
-  test.only("channels loads iframe", async ({ page }) => {
+  test("channels loads iframe", async ({ page }) => {
     await page.goto(`/channels/steveclaus`);
     const iframe = page.locator("iframe");
 
