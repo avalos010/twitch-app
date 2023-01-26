b5f7db4eba781201138c61dc1bb743855a1d634e
JLuis Avalos <luizavalos40@gmail.com>
2023-01-26 14:45:50 -0600

    started navigation

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,17 @@ import Channel from "./Routes/Channel";
 
 function App() {
   return (
-    <div className="container">
+    <div>
       <Nav />
-      <Routes>
-        <Route path="/" element={<Categories />} />
-        <Route path="/clips/:id" element={<Clips />} />
-        <Route path="/clips/play/:id" element={<Clip />} />
-        <Route path="/channels" element={<Channels />} />
-        <Route path="/channels/:id" element={<Channel />} />
-      </Routes>
+      <div className="container mx-auto">
+        <Routes>
+          <Route path="/" element={<Categories />} />
+          <Route path="/clips/:id" element={<Clips />} />
+          <Route path="/clips/play/:id" element={<Clip />} />
+          <Route path="/channels" element={<Channels />} />
+          <Route path="/channels/:id" element={<Channel />} />
+        </Routes>
+      </div>
     </div>
   );
 }
--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -1,36 +1,64 @@
+import React from "react";
 import { Link, useLocation } from "react-router-dom";
 
 function Nav() {
   const { pathname } = useLocation();
 
+  // return (
+  //   <nav className="navbar navbar-expand navbar-light bg-light">
+  //     <div className="container-fluid">
+  //       <div className="collapse navbar-collapse" id="navbarNav">
+  //         <ul className="navbar-nav">
+  //           <li className="nav-item">
+  //             <Link
+  //               to="/"
+  //               className={pathname == "/" ? "nav-link active" : "nav-link"}
+  //               aria-current="page"
+  //             >
+  //               Home
+  //             </Link>
+  //           </li>
+  //           <li className="nav-item">
+  //             <Link
+  //               to="/channels"
+  //               className={
+  //                 pathname === "/channels" ? "nav-link active" : "nav-link"
+  //               }
+  //               aria-current="page"
+  //             >
+  //               Channels
+  //             </Link>
+  //           </li>
+  //         </ul>
+  //       </div>
+  //     </div>
+  //   </nav>
+  // );
+
+  const links = [
+    ["Categories", "/"],
+    ["Channels", "/channels"],
+  ];
+
+  console.log(pathname);
+
   return (
-    <nav className="navbar navbar-expand navbar-light bg-light">
-      <div className="container-fluid">
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link
-                to="/"
-                className={pathname == "/" ? "nav-link active" : "nav-link"}
-                aria-current="page"
-              >
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/channels"
-                className={
-                  pathname === "/channels" ? "nav-link active" : "nav-link"
-                }
-                aria-current="page"
-              >
-                Channels
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </div>
+    <nav className="flex justify-start space-x-2 p-3 bg-opacity-80 bg-gray-700 mb-3">
+      {React.Children.toArray(
+        links.map((item) => {
+          const [name, link] = item;
+          return (
+            <Link
+              className={`px-2 rounded-lg text-white ${
+                pathname === link ? "text-black" : ""
+              }`}
+              to={link}
+            >
+              {name}
+            </Link>
+          );
+        })
+      )}
     </nav>
   );
 }
