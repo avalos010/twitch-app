399a8222575178d74bad63f8e17255edc8a6ec91
JLuis Avalos <luizavalos40@gmail.com>
2023-01-26 19:11:12 -0600

    card to tailwind

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -4,13 +4,16 @@ function Card({ name, imageUrl, buttonText, id }: CardProps) {
   return (
     <div
       data-testid="card"
-      className="card d-flex p-3 text-center flex-column justify-content-between align-items-center"
+      className="flex p-3 text-center flex-col justify-center items-center shadow-slate-500 shadow-sm border-t relative h-100"
       style={{ width: "200px" }}
     >
-      <img src={imageUrl} className="img-thumbnail mt-4" alt="..." width={80} />
+      <img src={imageUrl} className="mt-4" alt="..." width={80} />
 
-      <h5>{name}</h5>
-      <Link to={`/clips/${id}`} className="btn btn-primary ">
+      <h5 className="text-lg leading-none m-2 mb-14">{name}</h5>
+      <Link
+        to={`/clips/${id}`}
+        className="bg-slate-700 rounded-sm text-white text-md hover:bg-black leading-none p-1 w-full absolute bottom-0 left-0 right-0 hover:shadow-md hover:shadow-slate-700"
+      >
         {buttonText}
       </Link>
     </div>
